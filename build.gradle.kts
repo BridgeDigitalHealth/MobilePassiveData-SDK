7f0172ee23306331a72851ae3e7b452e5abb3e9c
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-12-09 09:15:53 -0800

    Version 0.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 allprojects {
     group = "org.sagebionetworks.research.kmm"
-    version = "0.5.0"
+    version = "0.5.1"
     repositories {
         google()
         mavenCentral()
